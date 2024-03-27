3aaf0f1da4abdb2c64d4cf13e71c452a07a6773b
Artem <fdggfh@gmail.com>
2024-03-27 15:43:13 +0300

    garnish

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import './App.css';
 // import required modules
 import { Keyboard,  Navigation } from 'swiper/modules';
 import Shawarma from './components/shwarma/shawarma';
+import Garnish from './components/garnish/garnish';
 
 
 function App() {
@@ -30,14 +31,8 @@ function App() {
         className="mySwiper"
       >
         <SwiperSlide><Shawarma /></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide><Garnish /></SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
---
+++ src/components/garnish/garnish.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import s from './garnish.module.css'
+import pilafPic from '../../fonts/plov.png'
+
+const Garnish = () => {
+
+    const pilafs = [
+        {
+            name: 'Плов с курицей',
+            pic: pilafPic,
+            description: 'Курица, лук, перец, кумин, рис, кориандер',
+            weight: 500,
+            coast: 10,
+        },
+        {
+            name: 'Плов с говядиной',
+            pic: pilafPic,
+            description: 'Говядина, лук, перец, кумин, рис, чеснок',
+            weight: 500,
+            coast: 12.5,
+        },
+        {
+            name: 'Комплекс с курицей',
+            pic: pilafPic,
+            description: 'Курица, картофель фри, свежие огурцы и помидоры, соус',
+            weight: 450,
+            coast: 12.5,
+        },
+    ]
+
+    return (
+        <>
+            {
+                pilafs.map((item) => (
+                    <div className={s.mainPilaf}>
+                        <div className={s.img}>
+                            <img src={item.pic} alt={item.name} />
+                        </div>
+                        <h2 className={s.mainName}>{item.name}</h2>
+                        <div className={s.ingridients}>
+                            <h3>{item.description}</h3>
+                        </div>
+                        <button className={s.button}>
+                            <h4>{item.coast}</h4>
+                            <h5>{item.weight}</h5>
+                        </button>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default Garnish
