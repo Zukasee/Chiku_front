cf234d3df5931bd1bcb5d00b8601e19ad3646aba
Artem <fdggfh@gmail.com>
2024-04-12 00:52:57 +0300

    navigate

--- src/components/form/from.tsx
+++ src/components/form/from.tsx
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react'
 import s from './form.module.css'
 import { userContext } from '../../App'
+import { useNavigate } from 'react-router-dom'
 
 const Form = () => {
 
+    const navigate = useNavigate()
     const {order, setOrder} = useContext(userContext)
     setOrder(order)
 
+
+    const toOrder = () => {
+        navigate('/order')
+    }
+
     return (
         <>
+            <div className={s.header}>
+                <h2>Запоните данный:</h2>
+                <h4 onClick={toOrder}>редактировать заказ</h4>
+            </div>
             <form className={s.form}>
                 <label className={s.field_item}>
                     <input type='text' required />
