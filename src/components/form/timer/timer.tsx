fabf84fc70a30885b89a670d4d4b44891ed78e2f
Artem <fdggfh@gmail.com>
2024-04-12 17:39:02 +0300

    css

--- src/components/form/timer/timer.tsx
+++ src/components/form/timer/timer.tsx
@@ -33,7 +33,7 @@ const Timer = () => {
 
     return (
         <>
-            Время выдачи заказа
+            <h2 className={s.top}>Выберите время выдачи заказа</h2>
             <div className={s.times}>
                 {times.map((timeObject, index) => (
                     <div key={index} className={`${s.time} ${index === selectedTime ? s.selectedTime : ''}`} onClick={() => handleClickTime(index)}>
